e6f3c15d984808de67807ac26d4679a74145e4de
Braio <mernickbryan@gmail.com>
2024-06-02 22:14:06 -0300

    fix: correção de cores dos botões iniciais

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -24,6 +24,10 @@
             border: none;
             border-radius: 0;
         }
+
+        i{
+            color: #fff;
+        }
     </style>
 </head>
 
@@ -49,7 +53,7 @@
                 </button>
             </a>
             <a href="/trabalhe-conosco">
-                <button type="button" class="btn btn-light btn-final mb-2">
+                <button type="button" class="btn btn-info btn-final mb-2">
                     <i class="fas fa-users mr-1"></i> Trabalhe conosco
                 </button>
             </a>
--- resources/views/pages/simular.blade.php
+++ resources/views/pages/simular.blade.php
@@ -152,7 +152,7 @@
                     </button>
                 </a>
                 <a href="/trabalhe-conosco">
-                    <button type="button" class="btn btn-light mb-2">
+                    <button type="button" class="btn btn-info mb-2">
                         <i class="fas fa-users mr-1"></i> Trabalhe conosco
                     </button>
                 </a>
